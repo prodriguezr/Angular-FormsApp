4e59d00475e595d3c47d18351676dab20b41d7e5
Pablo Rodriguez <prodrigu75@gmail.com>
2021-05-02 16:28:24 -0400

    Reactive forms basics ended

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BasicsComponent {
+export class BasicsComponent implements OnInit {
+  myForm:FormGroup = this.formBuilder.group({
+    name: [ , [Validators.required, Validators.minLength(3)] ],
+    price: [ , [ Validators.required, Validators.min(0) ] ],
+    stock: [ , [ Validators.required, Validators.min(0) ] ],
+  });
+
+  constructor(private formBuilder:FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm.reset({
+      name: 'Coca-Cola Zero',
+      price: 0,
+    });
+  }
+
+  showError(field:string):boolean {
+    return (this.myForm.controls[field].errors === null ? false : true )
+      && this.myForm.controls[field].touched; 
+  }
+
+  save():void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
