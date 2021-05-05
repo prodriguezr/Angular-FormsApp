410c6610209a74ec3198f3649741bc065e550d7b
Pablo Rodriguez <prodrigu75@gmail.com>
2021-05-05 18:50:58 -0400

    Reactive forms switches ended

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+enum Gender {
+  F, M
+}
+
+interface Person {
+  gender: string,
+  notify: boolean,
+}
 
 @Component({
   selector: 'app-switches',
@@ -7,10 +17,41 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SwitchesComponent implements OnInit {
+  myForm:FormGroup = this.formBuilder.group({
+    gender: [ 'M', Validators.required ],
+    notify: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  person:Person = {
+    gender: 'F',
+    notify: true,
   }
 
+  constructor(private formBuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset({
+      ...this.person, 
+      termsAndConditions: false 
+    });
+
+    this.myForm.valueChanges.subscribe(({ termsAndConditions, ...rest }) => {
+      //delete myForm.termsAndConditions;
+      this.person = rest;
+    });
+
+    this.myForm.get('termsAndConditions')?.valueChanges.subscribe(newValue => {
+      console.log(newValue);
+    });
+  }
+
+  save():void {
+    const formValue = { ...this.myForm.value };
+    delete formValue.termsAndConditions;
+
+    this.person = formValue;
+
+    console.log(formValue);
+  }
 }
