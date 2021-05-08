55bab7b5b6b9c26fe98d0fd4c554422ce75adc21
Pablo Rodriguez <prodrigu75@gmail.com>
2021-05-08 16:12:06 -0400

    Auth module and components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ const routes: Routes = [
   { path: 'template',
     loadChildren: () => import('./template/template.module').then( m => m.TemplateModule)
   },
+  { path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+  },
   {
     path: '**',
     redirectTo: 'template',
